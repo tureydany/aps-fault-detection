cabb2fb3e578039143f2fb3ae461501d56ebc65e
Tuery <tureydany19@gmail.com>
2022-12-14 23:24:38 +0530

    deployment

--- data_dump.py
+++ data_dump.py
@@ -1,8 +1,9 @@
 import pymongo
 import pandas as pd
 import json
+from sensor.config import mongo_client
 # Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+#client = pymongo.MongoClient("mongodb+srv://turey_dany:arul2304@cluster0.egy97.mongodb.net/?retryWrites=true&w=majority")
 
 DATABASE_NAME="aps"
 Collection_name="sensor"
@@ -16,4 +17,4 @@ if __name__=="__main__":
     json_record=list(json.loads(df.T.to_json()).values())     #loads() method can be used to parse a valid JSON string and convert it into a Python Dictionary
     print(json_record[0])
 
-    client[DATABASE_NAME][Collection_name].insert_many(json_record)
+    mongo_client[DATABASE_NAME][Collection_name].insert_many(json_record)
--- sensor/config.py
+++ sensor/config.py
@@ -12,9 +12,6 @@ class EnvironmentVariable:
     aws_access_secret_key:str = os.getenv("AWS_SECRET_ACCESS_KEY")
 
 
-
-
-
 env_var = EnvironmentVariable()
 mongo_client = pymongo.MongoClient(env_var.mongo_db_url)
 TARGET_COLUMN = "class"
